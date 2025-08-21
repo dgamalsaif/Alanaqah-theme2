032619eb3798f688500e7eeeffe27005200f4863
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-21 22:29:44 +0300

    Create archive-product.php

---
+++ woocommerce/archive-product.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * The Template for displaying product archives, including the main shop page.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ *
+ * @package AlamAlAnika/WooCommerce
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header( 'shop' );
+
+/**
+ * Hook: woocommerce_before_main_content.
+ *
+ * @hooked woocommerce_output_content_wrapper - 10 (we will remove this)
+ * @hooked woocommerce_breadcrumb - 20
+ * @hooked woocommerce_output_all_notices - 10
+ */
+do_action( 'woocommerce_before_main_content' );
+?>
+<div class="category-page active" id="main-content">
+    <div class="category-header">
+        <div class="breadcrumb-container">
+            <?php woocommerce_breadcrumb(); ?>
+        </div>
+        <div class="category-title-section">
+            <h1 class="category-title woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+            <?php
+            /**
+             * Hook: woocommerce_archive_description.
+             *
+             * @hooked woocommerce_taxonomy_archive_description - 10
+             * @hooked woocommerce_product_archive_description - 10
+             */
+            do_action( 'woocommerce_archive_description' );
+            ?>
+        </div>
+    </div>
+    
+    <div class="category-container">
+        <aside class="filter-sidebar">
+            <?php
+            /**
+             * Hook: woocommerce_sidebar.
+             *
+             * @hooked woocommerce_get_sidebar - 10
+             */
+            do_action( 'woocommerce_sidebar' );
+            ?>
+        </aside>
+        
+        <div class="products-section">
+            <div class="products-header">
+                <?php
+                /**
+                 * Hook: woocommerce_before_shop_loop.
+                 *
+                 * @hooked woocommerce_output_all_notices - 10
+                 * @hooked woocommerce_result_count - 20
+                 * @hooked woocommerce_catalog_ordering - 30
+                 */
+                do_action( 'woocommerce_before_shop_loop' );
+                ?>
+            </div>
+            
+            <?php
+            if ( woocommerce_product_loop() ) {
+
+                woocommerce_product_loop_start();
+
+                if ( wc_get_loop_prop( 'total' ) ) {
+                    while ( have_posts() ) {
+                        the_post();
+                        
+                        /**
+                         * Hook: woocommerce_shop_loop.
+                         */
+                        do_action( 'woocommerce_shop_loop' );
+
+                        wc_get_template_part( 'content', 'product' );
+                    }
+                }
+
+                woocommerce_product_loop_end();
+
+                /**
+                 * Hook: woocommerce_after_shop_loop.
+                 *
+                 * @hooked woocommerce_pagination - 10
+                 */
+                do_action( 'woocommerce_after_shop_loop' );
+            } else {
+                /**
+                 * Hook: woocommerce_no_products_found.
+                 *
+                 * @hooked wc_no_products_found - 10
+                 */
+                do_action( 'woocommerce_no_products_found' );
+            }
+            ?>
+        </div>
+    </div>
+</div>
+
+<?php
+/**
+ * Hook: woocommerce_after_main_content.
+ *
+ * @hooked woocommerce_output_content_wrapper_end - 10 (we will remove this)
+ */
+do_action( 'woocommerce_after_main_content' );
+
+get_footer( 'shop' );
