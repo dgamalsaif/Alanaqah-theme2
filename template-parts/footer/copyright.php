2c95783aed508e08b4ca9545de751aef53ca1421
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-21 20:37:29 +0300

    Create copyright.php

---
+++ template-parts/footer/copyright.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The template part for displaying the footer copyright information.
+ *
+ * @package AlamAlAnika
+ */
+?>
+<div class="footer-bottom">
+    <p>&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'جميع الحقوق محفوظة.', 'alam-al-anika' ); ?></p>
+</div><!-- .footer-bottom -->
