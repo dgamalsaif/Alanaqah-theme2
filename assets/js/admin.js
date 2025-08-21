d221cd09f09ae0ac28df992df6de97abf623f114
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-21 21:22:16 +0300

    Add files via upload

---
+++ assets/js/admin.js
@@ -0,0 +1,14 @@
+/**
+ * JavaScript for the admin area.
+ *
+ * Initializes the color picker on the attribute term edit screen.
+ */
+(function($) {
+    'use strict';
+
+    $(function() {
+        // Initialize the WordPress color picker
+        $('.alam-al-anika-color-picker').wpColorPicker();
+    });
+
+})(jQuery);
