c586d1622031c313c8028abfe50d8cda31437dbd
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-23 01:26:48 +0300

    Add files via upload

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1,1 +1,64 @@
- <?php /** * The template for displaying single product content. * @package AlamAlAnika/WooCommerce */ defined( 'ABSPATH' ) || exit; ?> <div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'product-detail-page active', $product ); ?>> <div class="product-breadcrumb"> <?php woocommerce_breadcrumb(); ?> </div> <div class="product-detail-container"> <?php // Corrected Paths: get_template_part('template-parts/single/product-images'); get_template_part('template-parts/single/product-info'); ?> </div> <?php // Corrected Path: get_template_part('template-parts/single/related-products'); ?> </div> <?php do_action( 'woocommerce_after_single_product' ); ?> 
+<?php
+/**
+ * The Template for displaying all single products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header( 'shop' ); ?>
+
+	<?php
+		/**
+		 * Woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+	<?php
+	while ( have_posts() ) :
+		the_post();
+		?>
+
+		<?php wc_get_template_part( 'content', 'single-product' ); ?>
+
+	<?php endwhile; // end of the loop. ?>
+
+	<?php
+		/**
+		 * Woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+	<?php
+		/**
+		 * Woocommerce_sidebar hook.
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
+	?>
+
+<?php
+get_footer( 'shop' );
+
+/* Omit closing PHP tag to prevent "headers already sent" issues. */
