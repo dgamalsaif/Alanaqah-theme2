e5f996f98a2b5bc823a9befba80374b83e3ab4d1
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-23 02:00:56 +0300

    Add files via upload

--- template-parts/footer/widgets.php
+++ template-parts/footer/widgets.php
@@ -1,61 +1,34 @@
 <?php
 /**
- * The template part for displaying the footer widgets area.
+ * The template for displaying the footer widgets.
  *
  * @package AlamAlAnika
  */
+
 ?>
+
 <div class="footer-content">
-    <div class="footer-column">
-        <h3><?php esc_html_e( 'خدمة العملاء', 'alam-al-anika' ); ?></h3>
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'footer-1',
-                'menu_id'        => 'footer-menu-1',
-                'container'      => 'ul',
-                'fallback_cb'    => false,
-            )
-        );
-        ?>
-    </div>
-    <div class="footer-column">
-        <h3><?php esc_html_e( 'من نحن', 'alam-al-anika' ); ?></h3>
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'footer-2',
-                'menu_id'        => 'footer-menu-2',
-                'container'      => 'ul',
-                'fallback_cb'    => false,
-            )
-        );
-        ?>
-    </div>
-    <div class="footer-column">
-        <h3><?php esc_html_e( 'مساعدة', 'alam-al-anika' ); ?></h3>
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'footer-3',
-                'menu_id'        => 'footer-menu-3',
-                'container'      => 'ul',
-                'fallback_cb'    => false,
-            )
-        );
-        ?>
-    </div>
-    <div class="footer-column">
-        <h3><?php esc_html_e( 'تواصل معنا', 'alam-al-anika' ); ?></h3>
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'footer-4',
-                'menu_id'        => 'footer-menu-4',
-                'container'      => 'ul',
-                'fallback_cb'    => false,
-            )
-        );
-        ?>
-    </div>
-</div><!-- .footer-content -->
+	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+		<div class="footer-column">
+			<?php dynamic_sidebar( 'footer-1' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'footer-2' ) ) : ?>
+		<div class="footer-column">
+			<?php dynamic_sidebar( 'footer-2' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'footer-3' ) ) : ?>
+		<div class="footer-column">
+			<?php dynamic_sidebar( 'footer-3' ); ?>
+		</div>
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'footer-4' ) ) : ?>
+		<div class="footer-column">
+			<?php dynamic_sidebar( 'footer-4' ); ?>
+		</div>
+	<?php endif; ?>
+</div>
