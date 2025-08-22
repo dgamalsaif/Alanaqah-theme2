040b26b6982c7a55ef4e34b152c4b851abdcf6cc
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-23 01:39:22 +0300

    Add files via upload

--- inc/widgets.php
+++ inc/widgets.php
@@ -1,70 +1,57 @@
 <?php
 /**
- * Custom Widgets for the Alam Al Anika Theme.
+ * Theme widgets.
  *
  * @package AlamAlAnika
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
-
 /**
- * Register custom widgets.
+ * Register widget area.
  *
- * This function is hooked into the 'widgets_init' action.
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function alam_al_anika_register_custom_widgets() {
-	// Example of how you would register a custom widget class:
-	// register_widget( 'Alam_Al_Anika_Example_Widget' );
-}
-add_action( 'widgets_init', 'alam_al_anika_register_custom_widgets' );
+function alam_al_anika_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Sidebar', 'alam-al-anika' ),
+			'id'            => 'sidebar-1',
+			'description'   => esc_html__( 'Add widgets here.', 'alam-al-anika' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 
-/**
- * Example Custom Widget Class.
- *
- * You can uncomment and build upon this structure to create your own widgets.
- */
-/*
-class Alam_Al_Anika_Example_Widget extends WP_Widget {
-
-	// Constructor: Sets up the widget name, etc.
-	public function __construct() {
-		$widget_ops = array(
-			'classname'                   => 'alam_al_anika_example_widget',
-			'description'                 => __( 'An example widget for the theme.', 'alam-al-anika' ),
-			'customize_selective_refresh' => true,
+	// Footer widget areas.
+	for ( $i = 1; $i <= 4; $i++ ) {
+		register_sidebar(
+			array(
+				/* translators: %d: The number of the footer widget area. */
+				'name'          => sprintf( esc_html__( 'Footer %d', 'alam-al-anika' ), $i ),
+				'id'            => 'footer-' . $i,
+				'description'   => esc_html__( 'Add widgets here to appear in your footer.', 'alam-al-anika' ),
+				'before_widget' => '<div id="%1$s" class="footer-column widget %2$s">',
+				'after_widget'  => '</div>',
+				'before_title'  => '<h3 class="widget-title">',
+				'after_title'   => '</h3>',
+			)
 		);
-		parent::__construct( 'alam_al_anika_example_widget', __( 'Example Widget', 'alam-al-anika' ), $widget_ops );
 	}
 
-	// Outputs the content of the widget on the front-end.
-	public function widget( $args, $instance ) {
-		echo $args['before_widget'];
-		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
-		}
-		echo esc_html__( 'Hello, World!', 'alam-al-anika' );
-		echo $args['after_widget'];
-	}
-
-	// Outputs the options form on the admin side.
-	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'alam-al-anika' );
-		?>
-		<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'alam-al-anika' ); ?></label>
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
-		<?php
-	}
-
-	// Processes widget options to be saved.
-	public function update( $new_instance, $old_instance ) {
-		$instance          = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
-
-		return $instance;
+	// Shop sidebar for WooCommerce.
+	if ( class_exists( 'WooCommerce' ) ) {
+		register_sidebar(
+			array(
+				'name'          => esc_html__( 'Shop Sidebar', 'alam-al-anika' ),
+				'id'            => 'shop-sidebar',
+				'description'   => esc_html__( 'Widgets added here will appear on WooCommerce archive pages.', 'alam-al-anika' ),
+				'before_widget' => '<section id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</section>',
+				'before_title'  => '<h2 class="widget-title">',
+				'after_title'   => '</h2>',
+			)
+		);
 	}
 }
-*/
+add_action( 'widgets_init', 'alam_al_anika_widgets_init' );
