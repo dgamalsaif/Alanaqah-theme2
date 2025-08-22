e759f3a34860aae9c61586581033c920996d24b2
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-23 01:54:06 +0300

    Add files via upload

--- template-parts/homepage/flash-sale.php
+++ template-parts/homepage/flash-sale.php
@@ -1,52 +1,45 @@
 <?php
 /**
- * The template for displaying the flash sale section on the homepage.
+ * Template part for displaying the flash sale section on the homepage.
  *
  * @package AlamAlAnika
  */
 
-// Get IDs of products on sale
-$product_ids_on_sale = wc_get_product_ids_on_sale();
+$args = array(
+	'post_type'      => 'product',
+	'posts_per_page' => 4,
+	'meta_key'       => 'total_sales', // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+	'orderby'        => 'meta_value_num',
+	'order'          => 'DESC',
+);
 
-if ( ! empty( $product_ids_on_sale ) ) :
+$query = new WP_Query( $args );
 ?>
-<section class="flash-sale section">
-    <div class="flash-header">
-        <div class="flash-title">
-            <div class="flash-icon">
-                <i class="fas fa-bolt"></i>
-            </div>
-            <h2><?php esc_html_e( 'عروض سريعة', 'alam-al-anika' ); ?></h2>
-        </div>
-        <div class="flash-timer">
-            <i class="far fa-clock"></i>
-            <span id="flash-timer"><?php esc_html_e( 'ينتهي خلال 05:53:48', 'alam-al-anika' ); ?></span>
-        </div>
-    </div>
 
-    <div class="product-grid">
-        <?php
-        $args = array(
-            'post_type'      => 'product',
-            'posts_per_page' => 4, // Show 4 products in this section
-            'post__in'       => $product_ids_on_sale,
-        );
-        $flash_sale_query = new WP_Query( $args );
-
-        if ( $flash_sale_query->have_posts() ) :
-            // We need to use the WooCommerce loop structure here
-            echo '<ul class="products columns-4">';
-            while ( $flash_sale_query->have_posts() ) :
-                $flash_sale_query->the_post();
-                wc_get_template_part( 'content', 'product' );
-            endwhile;
-            echo '</ul>';
-        else :
-            echo '<p>' . esc_html__( 'No products are currently on sale.', 'alam-al-anika' ) . '</p>';
-        endif;
-        wp_reset_postdata();
-        ?>
-    </div>
+<section class="section flash-sale">
+	<div class="section-header">
+		<h2 class="section-title"><?php esc_html_e( 'Flash Sale', 'alam-al-anika' ); ?></h2>
+		<a href="#" class="view-all"><?php esc_html_e( 'View All', 'alam-al-anika' ); ?> <i class="fas fa-chevron-left"></i></a>
+	</div>
+	<?php if ( $query->have_posts() ) : ?>
+		<ul class="products">
+			<?php
+			while ( $query->have_posts() ) :
+				$query->the_post();
+				global $product;
+				?>
+				<li class="product">
+					<a href="<?php echo esc_url( get_the_permalink() ); ?>">
+						<?php echo wp_kses_post( $product->get_image() ); ?>
+						<h2 class="woocommerce-loop-product__title"><?php echo esc_html( get_the_title() ); ?></h2>
+						<span class="price"><?php echo wp_kses_post( $product->get_price_html() ); ?></span>
+					</a>
+					<?php woocommerce_template_loop_add_to_cart(); ?>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+	<?php else : ?>
+		<p><?php esc_html_e( 'No flash sale products at the moment.', 'alam-al-anika' ); ?></p>
+	<?php endif; ?>
+	<?php wp_reset_postdata(); ?>
 </section>
-<?php
-endif;
