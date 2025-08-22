d4c2832560593f00dad979074ecb379d92f2325d
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-22 22:10:45 +0300

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,16 +1,1 @@
-```javascript name=assets/js/admin.js
-/**
- * JavaScript for the admin area.
- *
- * Initializes the color picker on the attribute term edit screen.
- */
-(function($) {
-    'use strict';
-
-    $(function() {
-        // Initialize the WordPress color picker
-        $('.alam-al-anika-color-picker').wpColorPicker();
-    });
-
-})(jQuery);
-```
+ 
