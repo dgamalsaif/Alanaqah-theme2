ae5aeee10167d9901893aedaeeb1b07bc134ad8f
dgamalsaif <142757236+dgamalsaif@users.noreply.github.com>
2025-08-23 01:56:01 +0300

    Add files via upload

--- template-parts/footer/copyright.php
+++ template-parts/footer/copyright.php
@@ -1,10 +1,20 @@
 <?php
 /**
- * The template part for displaying the footer copyright information.
+ * The template for displaying the footer copyright.
  *
  * @package AlamAlAnika
  */
 ?>
+
 <div class="footer-bottom">
-    <p>&copy; <?php echo esc_html( date( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?>. <?php esc_html_e( 'جميع الحقوق محفوظة.', 'alam-al-anika' ); ?></p>
-</div><!-- .footer-bottom -->
+	<p>
+		<?php
+		/* translators: 1: Current year, 2: Site name */
+		printf(
+			esc_html__( '&copy; %1$s %2$s. All Rights Reserved.', 'alam-al-anika' ),
+			esc_html( date_i18n( 'Y' ) ),
+			esc_html( get_bloginfo( 'name' ) )
+		);
+		?>
+	</p>
+</div>
